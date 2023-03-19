3029ddd5be9f3fcaf77a122accd9f6f531103130
XxHiroTTVxX <101534563+XxHiroTTVxX@users.noreply.github.com>
2023-03-19 10:38:30 +0000

    idk

--- theme.config.tsx
+++ theme.config.tsx
@@ -3,6 +3,13 @@ import { DocsThemeConfig } from "nextra-theme-docs";
 
 const config: DocsThemeConfig = {
   logo: <span>Ruby Docs</span>,
+
+  head: (
+    <React.Fragment>
+      <link rel="icon" type="image/png" href="./favicon.png" />
+    </React.Fragment>
+  ),
+
   project: {
     link: "https://github.com/Ruby-Network/ruby-docs",
   },
@@ -11,8 +18,10 @@ const config: DocsThemeConfig = {
   },
   docsRepositoryBase: "https://github.com/Ruby-Network/ruby-docs/tree/main",
   footer: {
-    text: "Nextra Docs Template",
+    text: "&copy; 2021 Ruby Network",
   },
+  
+  
 };
 
 export default config;
